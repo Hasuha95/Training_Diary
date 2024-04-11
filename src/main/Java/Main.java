1ea4c20eec76b31c9f54c33e98fc998a432f9b87
Gorets <xacyxanovich@mail.ru>
2024-04-11 14:58:18 +0300

    auth. ready

--- src/main/Java/Main.java
+++ src/main/Java/Main.java
@@ -6,46 +6,83 @@ import java.util.Scanner;
 public class Main {
     static final UserService userService = new SimpleUserService();
     static final Scanner scan = new Scanner(System.in);
+    static DiaryUser currentUser;
 
     public static void main(String[] args) {
         int choice;
-        System.out.println("\n-------------------");
-        System.out.println("TRAINING DIARY");
-        System.out.println("-------------------\n");
+        userService.createNewUser(
+                new DiaryUser("GrandAdmin",
+                "adminLogin",
+                "admin666",
+                "Admin")
+        );
 
-        boolean authenticated = false;
         while (true) {
-            if (!authenticated) {
+            System.out.println("\n-------------------");
+            System.out.println("TRAINING DIARY");
+            System.out.println("-------------------\n");
+            if (currentUser == null) {
                 System.out.println("1. Registration.");
                 System.out.println("2. Authorization.");
                 System.out.println("3. Help.");
                 System.out.print("\nEnter your choice : ");
-            }
-            choice = scan.nextInt();
-            switch (choice) {
-                case 1: registration();
+                choice = scan.nextInt();
+                switch (choice) {
+                    case 1: registration();
+                    break;
+                    case 2: authorization();
+                    break;
+                    case 3: help();
+                    break;
+                }
             }
         }
     }
 
-    public static void registration(){
-        System.out.print("Enter your Name : ");
-        String name = scan.nextLine();
-        System.out.print("Enter login : ");
-        String userName = scan.nextLine();
+    private static void registration(){
+        System.out.println("_Registration_");
+        System.out.println("Enter your Name : ");
+        String name = scan.next();
+        System.out.println("Enter login : ");
+        String userName = scan.next();
         while (userService.getUserByUserName(userName) != null) {
             System.out.println("Username already exists. Set again : ");
             userName = scan.next();
         }
         System.out.println("Enter password:");
         String password = scan.next();
-        System.out.println("Choose role: "+ "\nUser or Admin");
-        String role = scan.nextLine();
-        while (role!="User" || role!="Admin"){
-            System.out.println("Please choose correct role");
-            role = scan.nextLine();
+        System.out.println("Choose role: User or Admin");
+        String role = scan.next();
+        while (!role.equals("User") && !role.equals("Admin")){
+            System.out.println("Please write correct role");
+            role = scan.next();
         }
-        userService.createNewUser(new DiaryUser(name, userName, password, role));
+        currentUser = new DiaryUser(name, userName, password, role);
+        userService.createNewUser(currentUser);
+    }
+
+    private static void authorization() {
+        System.out.println("_Authorization_");
+        System.out.print("Enter login : ");
+        String userName = scan.next();
+        DiaryUser user = userService.getUserByUserName(userName);
+        while (user == null) {
+            System.out.println("User is not found. Try again : ");
+            userName = scan.next();
+            user = userService.getUserByUserName(userName);
+        }
+        System.out.print("Enter password : ");
+        String password = scan.next();
+        while (!user.getPassword().equals(password)) {
+            System.out.println("Incorrect password. Try again : ");
+            password = scan.next();
+        }
+        currentUser = user;
+    }
+
+    @Deprecated
+    private static void help() {
+
     }
 
 }
